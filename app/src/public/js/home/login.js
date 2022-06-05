b529c2cad479f49e693650b4b39f3aff7ea2cef0
SunghaeAn <ahshss903@gmail.com>
2022-06-06 02:48:49 +0900

    2206060248

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -17,5 +17,7 @@ function login() {
             "Content-Type": "application/json",
         },
         body: JSON.stringify(req),
-    });
+    })
+    .then((res) => res.json())
+    .then(console.log);
 }
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -9,10 +9,30 @@ const output= {
     }
 }
 
+const users = {
+    id: ["ahshss903"],
+    psword: ["1234"],
+};
+
 const process = {
     login: (req, res) => {
-        console.log(req.body);
-    }
+        const id = req.body.id;
+        const psword = req.body.psword;
+
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.psword[idx] === psword) {
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인에 실패하셨습니다.",
+        })
+    },
 }
 
 module.exports = {
