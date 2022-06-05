0862cb30fbf2c3bbc07da8479c74e84e552e427e
SunghaeAn <ahshss903@gmail.com>
2022-06-06 02:00:58 +0900

    220606 Update

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,15 @@
 "use strict";
 
-console.log("Connect");
+const id = document.querySelector("#id");
+const psword = document.querySelector("#pw");
+const loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        psword: psword.value,
+    };
+    console.log(req)
+}
