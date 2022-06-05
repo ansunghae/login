60e983fef6f0648903c8fcc40df880364215951c
SunghaeAn <ahshss903@gmail.com>
2022-06-06 02:56:36 +0900

    프론트엔드 로그인기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -19,5 +19,14 @@ function login() {
         body: JSON.stringify(req),
     })
     .then((res) => res.json())
-    .then(console.log);
+    .then((res) => {
+        if (res.success) {
+            location.href = "/"
+        }else{
+            alert(res.msg)
+        }
+    })
+    .catch((err) => {
+        console.error("로그인 중 에러 발생");
+    })
 }
