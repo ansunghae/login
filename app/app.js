c6e1063723ef76dcb0b8426235c18d794f0e8bd3
SunghaeAn <ahshss903@gmail.com>
2022-06-06 02:30:41 +0900

    2206060230

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@
 //Module
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 //Routing
 const home = require("./src/routes/home");
@@ -11,6 +12,8 @@ const home = require("./src/routes/home");
 app.set("views", "./app/src/views");
 app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use("/", home);
 
